f5518555a8b7c096bd8e53bbea5f58ed3faeb575
Nick Boughton <nicholasboughton@gmail.com>
2024-06-28 22:59:44 +0100

    Fixed compact stats layout

---
+++ src/components/Widgets/IconBox.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row items-center justify-center icon-box no-wrap">
+    <q-icon class="col-shrink" :name="icon" />
+    <span class="col-shrink">{{ text }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  icon: string;
+  text: string | number;
+}>();
+</script>
+
+<style scoped>
+.icon-box {
+  font-size: 1.5em;
+  font-weight: bold;
+}
+</style>
--- src/components/Widgets/StatsCompact.vue
+++ src/components/Widgets/StatsCompact.vue
@@ -1,38 +1,36 @@
 <template>
-  <div class="row justify-evenly items-center q-mt-sm">
-    <div class="col text-bold text-center text-h6" v-if="$q.screen.gt.xs">
-      <q-icon name="mdi-sword" />+{{ app.char.attacks.base }}
+  <div class="row items-center justify-between q-mt-sm q-px-md">
+    <div class="col-shrink" v-if="$q.screen.gt.xs">
+      <icon-box icon="mdi-sword" :text="`+${app.char.attacks.base}`" />
     </div>
 
-    <div class="col text-bold text-center text-h6" v-if="$q.screen.gt.xs">
-      <q-icon name="mdi-shield" />{{ +app.char.defense.base + modTotal(app.char.defense.mods) }}
+    <div class="col-shrink" v-if="$q.screen.gt.xs">
+      <icon-box icon="mdi-shield" :text="+app.char.defense.base + modTotal(app.char.defense.mods)" />
     </div>
 
-    <div v-for="(a, i) in app.char.aptitudes" :key="i" class="col">
+    <div v-for="(a, i) in app.char.aptitudes" :key="i" class="col-shrink">
       <stat-box :name="a.name" :value="+a.base + a.trait + modTotal(a.mods)" />
     </div>
 
-    <div class="col text-bold text-center text-h6" v-if="$q.screen.gt.xs">
-      <q-icon name="mdi-heart" color="red" />{{ +app.char.hearts.base + modTotal(app.char.hearts.mods) }}
+    <div class="col-shrink" v-if="$q.screen.gt.xs">
+      <icon-box icon="mdi-heart" color="red" :text="+app.char.hearts.base + modTotal(app.char.hearts.mods)" />
     </div>
 
-    <div class="col text-bold text-center text-h6" v-if="$q.screen.gt.xs">
-      <q-icon name="mdi-run" />{{ app.char.speed.selected }}
-    </div>
+    <div class="col-shrink" v-if="$q.screen.gt.xs"><icon-box icon="mdi-run" :text="app.char.speed.selected" /></div>
   </div>
 
-  <div class="row items-center justify-evenly" v-if="$q.screen.lt.sm">
-    <div class="col text-bold text-center text-h6"><q-icon name="mdi-sword" />+{{ app.char.attacks.base }}</div>
+  <div class="row justify-between q-px-md q-mt-sm" v-if="$q.screen.lt.sm">
+    <div class="col-shrink"><icon-box icon="mdi-sword" :text="app.char.attacks.base" /></div>
 
-    <div class="col text-bold text-center text-h6">
-      <q-icon name="mdi-shield" />{{ +app.char.defense.base + modTotal(app.char.defense.mods) }}
+    <div class="col-shrink">
+      <icon-box icon="mdi-shield" :text="+app.char.defense.base + modTotal(app.char.defense.mods)" />
     </div>
 
-    <div class="col text-bold text-center text-h6">
-      <q-icon name="mdi-heart" color="red" />{{ +app.char.hearts.base + modTotal(app.char.hearts.mods) }}
+    <div class="col-shrink">
+      <icon-box icon="mdi-heart" color="red" :text="+app.char.hearts.base + modTotal(app.char.hearts.mods)" />
     </div>
 
-    <div class="col text-bold text-center text-h6"><q-icon name="mdi-run" />{{ app.char.speed.selected }}</div>
+    <div class="col-shrink"><icon-box icon="mdi-run" :text="app.char.speed.selected" /></div>
   </div>
 </template>
 
@@ -42,6 +40,7 @@ import { useBreakStore } from 'src/stores/break-store';
 import { modTotal } from 'src/lib/util';
 
 import StatBox from './StatBox.vue';
+import IconBox from './IconBox.vue';
 
 const app = useBreakStore();
 </script>
